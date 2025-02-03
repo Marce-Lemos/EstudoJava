c2a388b0edf72bf0cf0789f82e86c10a196fdec5
Marcelo Lemos <164798762+Marce-Lemos@users.noreply.github.com>
2025-02-03 09:49:37 -0300

    adicionado arquivos antigos

---
+++ untitled/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+
+    Double livro1 = 59.9 ;
+    Double livro2 = 59.9;
+
+    Double soma = livro1 + livro2;
+    System.out.println("O valor total é de: " + soma);
+
+}
